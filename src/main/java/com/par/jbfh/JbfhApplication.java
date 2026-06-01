0023907f4c7fcdb87d972e3837f2a05fd9ad229a
Dima <prokuratdm@gmail.com>
2026-06-01 15:30:22 +0200

    Initial commit

---
+++ src/main/java/com/par/jbfh/JbfhApplication.java
@@ -0,0 +1,13 @@
+package com.par.jbfh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbfhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbfhApplication.class, args);
+    }
+
+}
